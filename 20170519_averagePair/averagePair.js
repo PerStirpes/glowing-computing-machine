3c53f65201cca88b1a8b74a3f02ff6e6982dd474
Matt <mccarl@asu.edu>
2017-08-20 23:10:24 -0700

    Update averagePair.js

--- 20170519_averagePair/averagePair.js
+++ 20170519_averagePair/averagePair.js
@@ -33,3 +33,17 @@ function averagePair(arr,target){
   }
   return false;
 }
+
+//instructor 
+function averagePair(arr, num){
+  let start = 0
+  let end = arr.length-1;
+  while(start < end){
+    let avg = (arr[start]+arr[end]) / 2 
+    if(avg === num) return true;
+    else if(avg < num) start++
+    else end--
+  }
+  return false;
+}
+
