a267ea170a5b9891a6f1ec86abee689ebca9582b
Matt <mccarl@asu.edu>
2017-08-21 02:49:52 -0700

    Update reverseVowels.js

--- 20170715_reverseVowels/reverseVowels.js
+++ 20170715_reverseVowels/reverseVowels.js
@@ -11,24 +11,25 @@ The vowels does not include the letter "y".
 */
 
 function reverseVowels(str) {
-  let vowels = new Set(['a','e','i','o','u']);
-  let i = 0, j = str.length - 1;
-  let arr = str.split("");
-  while (i < j) {
-    if (vowels.has(arr[i].toLowerCase()) && vowels.has(arr[j].toLowerCase())) {
-      [arr[i], arr[j]] = [arr[j], arr[i]];
-      i++;
-      j--;
-    } else if (vowels.has(arr[i].toLowerCase())) {
-      j--;
-    } else if (vowels.has(arr[j].toLowerCase())) {
-      i++;
-    } else {
-      i++;
-      j--;
+    let vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+    let i = 0;
+    let j = str.length - 1;
+    let arr = str.split("");
+    while (i < j) {
+        if (vowels.has(arr[i].toLowerCase()) && vowels.has(arr[j].toLowerCase())) {
+            [ arr [ i ] , arr [ j ] ] = [arr[j], arr[i]];
+            i++;
+            j--;
+        } else if (vowels.has(arr[i].toLowerCase())) {
+            j--;
+        } else if (vowels.has(arr[j].toLowerCase())) {
+            i++;
+        } else {
+            i++;
+            j--;
+        }
     }
-  }
-  return arr.join("");
+    return arr.join("");
 }
 
 // reverseVowels('hello'); // "holle"
@@ -55,3 +56,16 @@ function reverseVowels(str) {
    
   return arr.join(""); 
 }
+
+
+/*
+const reverseVowels = str => {
+  let vowels = str.replace(/[^aeiou]/gi, '').split('');
+  // or vowels = str.match(/[aeiou]/gi) // returns the same as above unless no match
+  return str.replace(/[aeiou]/gi, _ => vowels.pop());
+  // you can pass a callback function to replace, so you can use a function that depends on the character
+  // _ is the same as passing in (), i.e., no arguments
+};
+
+*/
+
